d695fec55e324d8c62f43ba27be5bbded5856f83
Aldi Abdu Malik <aldiabdumalik98@gmail.com>
2025-03-22 22:30:30 +0700

    fx: rm swagger

--- src/server/index.ts
+++ src/server/index.ts
@@ -3,18 +3,6 @@ import { apiReference } from '@scalar/hono-api-reference';
 
 const app = new OpenAPIHono();
 
-app.doc31('/api/swagger.json', {
-  openapi: '3.1.0',
-  info: { title: 'Hono x Nextjs', version: '0.1.0' },
-});
-
-app.get(
-  '/api/scalar',
-  apiReference({
-    url: '/api/swagger.json',
-  })
-);
-
 const routes = app.route('/');
 
 export type AppType = typeof routes;
